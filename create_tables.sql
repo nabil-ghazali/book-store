11d4bca700cf030752d208fff697ac65ece46ff9
github.com/nabil-ghazali <nabil.dev.ghaz@hotmail.com>
2025-07-09 14:21:20 +0200

    création base de données et insertion des données

--- create_tables.sql
+++ create_tables.sql
@@ -5,6 +5,8 @@ CREATE TABLE theme (
     theme_name VARCHAR(100) NOT NULL
 );
 
+
+
 CREATE TABLE author (
     author_id INT PRIMARY KEY,
     first_name VARCHAR(50) NOT NULL,
@@ -15,18 +17,19 @@ CREATE TABLE author (
 CREATE TABLE book (
    book_id INT PRIMARY KEY,
    title VARCHAR(50) NOT NULL,
-   author VARCHAR(50) NOT NULL,
+   author_id VARCHAR(50) NOT NULL,
    price DECIMAL(15,2) NOT NULL,
    stock INT NOT NULL,
-   date_publication DATE,
+   publication_year DATE,
+   theme_id VARCHAR(50) NOT NULL,
    FOREIGN KEY(theme_id) REFERENCES theme(theme_id),
    FOREIGN KEY(author_id) REFERENCES author(author_id)
 );
 
-
+ 
 CREATE TABLE customer (
     customer_id INT PRIMARY KEY,
-    email VARCHAR(100) NOT NULL,
+    email VARCHAR(50) NOT NULL,
     join_date DATE
 );
 
@@ -44,7 +47,9 @@ CREATE TABLE review (
    review_id INT PRIMARY KEY,
    rating BYTE NOT NULL,
    comment TEXT,
+   review_date DATE NOT NULL,
+   book_id INT NOT NULL,
+   customer_id INT NOT NULL,
    FOREIGN KEY(book_id) REFERENCES book(book_id),
    FOREIGN KEY(customer_id) REFERENCES customer(customer_id)
 );
-
