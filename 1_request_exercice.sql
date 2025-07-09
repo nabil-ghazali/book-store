aa30b699feb7f8ad64dfc2ade3689f1de970ea08
github.com/nabil-ghazali <nabil.dev.ghaz@hotmail.com>
2025-07-09 15:47:16 +0200

    request_exercice1

--- 1_request_exercice.sql
+++ 1_request_exercice.sql
@@ -1,26 +1,27 @@
 -- 1. Afficher prénom et nom de tous les auteurs
-
+SELECT first_name, last_name FROM author
 
 -- 2. Afficher auteurs nés entre 1950 et 1980
-
+SELECT first_name, last_name, birth_year  FROM author WHERE birth_year BETWEEN 1950 AND 1980;
 
 -- 3. Afficher 3 clients les plus récents (par date d’inscription)
-
+SELECT * FROM customer ORDER BY join_date DESC LIMIT 3;
 
 -- 4. Liste commandes avec id et montant total renommé "Montant"
-
+SELECT order_id, customer_id, book_id, order_date, total_amount AS "Montant" FROM customer_order;
 
 -- 5. Livres avec stock < 20
-
+SELECT * FROM book WHERE stock < 20
 
 -- 6. Avis avec note >= 4, afficher note et commentaire
-
+SELECT rating, comment FROM review WHERE rating >= 4;
 
 -- 7. Titres des livres avec année de publication, tri décroissant par année
-
+SELECT title, publication_year FROM book ORDER BY publication_year DESC;
 
 -- 8. Commandes au mois de mars 2024
-
+SELECT * from customer_order  WHERE strftime('%Y-%m', order_date) = '2024-03';
 
 -- 9. Livres dont le titre contient "secret"
 
+SELECT * FROM book WHERE title LIKE '%secret%'
